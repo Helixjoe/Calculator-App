5603c7948bb9937526179c2905affcd9661856f8
Joel John <joeljohn5112@gmail.com>
2023-01-26 17:47:19 +0530

    regular calculator completed

--- calculator.js
+++ calculator.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  res.send("Thank you for trying!. Result is " + (num1 + num2));
+});
+
 app.listen(3000, function () {
   console.log("Server Started");
 });
